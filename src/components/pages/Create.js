ce20ab137b9e01f2aa692252b2d962f631277bd3
xebec19 <thakur190420@gmail.com>
2021-03-01 12:41:52 +0530

    setting database

--- src/components/pages/Create.js
+++ src/components/pages/Create.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import 'firebase/database';
+import {auth,db} from '../../config/firebase';
 import Nav from '../etc/Nav';
+
 import '../../App.css';
 import 'tachyons';
 
 function Create(){
+	const [name,setName] = useState('');
+	const [date,setDate] = useState('');
+	const [phone,setPhone] = useState('');
+	const [org,setOrg] = useState('Org1');
+	const [ratings,setRatings] = useState('');
+	const [err,setErr] = useState('');
+	const object = {
+	 name: name,
+	 date: date,
+	 phone: phone,
+	 org: org,
+	 ratings: ratings
+	}
+	console.log(auth().currentUser.email);
+	function handleSubmit(){
+		if(name === '' || date=== '' || phone < 10000000 || ratings === ''){
+			setErr('Invalid');
+			return 0;
+		}
+		else{
+			console.log(object);
+			setErr('');
+			db.child('child').push(
+				object,
+				err => {
+					if(err) console.log('Error while saving ',err);
+				}
+				)
+		}
+	}
+	if(!auth().currentUser.email)
+	{	try{
+		return <h1>Logged out</h1>;	
+	}
+	catch(err){
+		console.log(err)
+	}
+	} 
 	return(
 		<div>
 		<Nav />
@@ -20,19 +61,21 @@ function Create(){
 	        <tr className="stripe-dark">
 	          <td className="pa3">Name</td>
 	          <td className="pa3">
-	          <input type="text" />
+	          <input type="text" onChange={(e) => setName(e.target.value)}/>
 	          </td>
 	        </tr>
 	        <tr className="stripe-dark">
 	          <td className="pa3">Date</td>
 	          <td className="pa3">
-	          <input type="date" />
+	          <input type="date" 
+	          onChange={(e) => setDate(e.target.value)}/>
 	          </td>
 	        </tr>
 	        <tr className="stripe-dark">
 	          <td className="pa3">Phone Number</td>
 	          <td className="pa3">
-	          <input type="number" />
+	          <input type="number" 
+	          onChange={(e) => setPhone(e.target.value)}/>
 	          </td>
 	        </tr>
 	        <tr className="stripe-dark">
@@ -40,11 +83,11 @@ function Create(){
 	          <td className="pa3">
 	          <div className="dropdown">
 	          <button className="dropbtn">
-	          Organization</button>
+	          {org}</button>
 	          <div className="dropdown-content">
-	          <p>Org1</p>
-	          <p>Org2</p>
-	          <p>Org3</p>
+	          <p onClick={(e) => setOrg('Org1')} value="Org1">Org1</p>
+	          <p onClick={(e) => setOrg('Org2')} value="Org2">Org2</p>
+	          <p onClick={(e) => setOrg('Org3')} value="Org3">Org3</p>
 	          </div>
 	          </div>
 	          </td>
@@ -52,7 +95,9 @@ function Create(){
 	        <tr className="stripe-dark">
 	          <td className="pa3">Ratings</td>
 	          <td className="pa3">
-	          <input type="text" />
+	          <input type="number"
+	          max="5" min="0"
+	          onChange={(e) => setRatings(e.target.value)} />
 	          </td>
 	        </tr>
 	        
@@ -63,7 +108,11 @@ function Create(){
 	        <tr className="stripe-dark center fw6 tl pa3 bg-white">
 	          <td className="center fw6 tl pa3 bg-white"></td>
 	          <td className="center fw6 tl pa3 bg-white">
-	          <button>Submit</button>
+	          {err && <p style={{color:"red"}}>Invalid</p>}
+	          <button
+	          onClick={() => handleSubmit()}>
+	          Submit
+	          </button>
 	          </td>
 	        </tr>
 	      </tfoot>
